cfd9ecdbe25c878127c070874ab24731c0538ea4
ABDULKADİR <36498544+Abdulkadir-Muhendis@users.noreply.github.com>
2020-01-06 07:21:03 +0200

    CE400 - ADC Sampling and IIR Filtering
    Algorithms (Building Blocks) / Digital Filters / 16-bit MCU & DSC

---
+++ ce400_adc_iir_filter/firmware/adc_iir_filter.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,27 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# dspic33ep512gm710 configuration
+CND_ARTIFACT_DIR_dspic33ep512gm710=dist/dspic33ep512gm710/production
+CND_ARTIFACT_NAME_dspic33ep512gm710=adc_iir_filter.X.production.hex
+CND_ARTIFACT_PATH_dspic33ep512gm710=dist/dspic33ep512gm710/production/adc_iir_filter.X.production.hex
+CND_PACKAGE_DIR_dspic33ep512gm710=${CND_DISTDIR}/dspic33ep512gm710/package
+CND_PACKAGE_NAME_dspic33ep512gm710=adciirfilter.x.tar
+CND_PACKAGE_PATH_dspic33ep512gm710=${CND_DISTDIR}/dspic33ep512gm710/package/adciirfilter.x.tar
+# dspic33ep512mu810 configuration
+CND_ARTIFACT_DIR_dspic33ep512mu810=dist/dspic33ep512mu810/production
+CND_ARTIFACT_NAME_dspic33ep512mu810=adc_iir_filter.X.production.hex
+CND_ARTIFACT_PATH_dspic33ep512mu810=dist/dspic33ep512mu810/production/adc_iir_filter.X.production.hex
+CND_PACKAGE_DIR_dspic33ep512mu810=${CND_DISTDIR}/dspic33ep512mu810/package
+CND_PACKAGE_NAME_dspic33ep512mu810=adciirfilter.x.tar
+CND_PACKAGE_PATH_dspic33ep512mu810=${CND_DISTDIR}/dspic33ep512mu810/package/adciirfilter.x.tar
+# dspic33ep256gp506 configuration
+CND_ARTIFACT_DIR_dspic33ep256gp506=dist/dspic33ep256gp506/production
+CND_ARTIFACT_NAME_dspic33ep256gp506=adc_iir_filter.X.production.hex
+CND_ARTIFACT_PATH_dspic33ep256gp506=dist/dspic33ep256gp506/production/adc_iir_filter.X.production.hex
+CND_PACKAGE_DIR_dspic33ep256gp506=${CND_DISTDIR}/dspic33ep256gp506/package
+CND_PACKAGE_NAME_dspic33ep256gp506=adciirfilter.x.tar
+CND_PACKAGE_PATH_dspic33ep256gp506=${CND_DISTDIR}/dspic33ep256gp506/package/adciirfilter.x.tar
