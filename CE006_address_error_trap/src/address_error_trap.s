512e9436a5e96ffddf7a44ad594f5815d124939e
ABDULKADİR <36498544+Abdulkadir-Muhendis@users.noreply.github.com>
2020-01-10 07:49:23 +0200

    Add files via upload
    Address Error Traps for Easy Debugging

---
+++ CE006_address_error_trap/src/address_error_trap.s
@@ -0,0 +1,90 @@
+/**********************************************************************
+* © 2005 Microchip Technology Inc.
+*
+* FileName:        address_error_trap.s
+* Dependencies:    Header (.h/.inc) files if applicable, see below
+* Processor:       dsPIC30Fxxxx
+* Compiler:        MPLAB® C30 v3.00 or higher
+* IDE:             MPLAB® IDE v7.52 or later
+* Dev. Board Used: dsPICDEM 1.1 Development Board
+* Hardware Dependencies: None
+*
+* SOFTWARE LICENSE AGREEMENT:
+* Microchip Technology Incorporated ("Microchip") retains all ownership and 
+* intellectual property rights in the code accompanying this message and in all 
+* derivatives hereto.  You may use this code, and any derivatives created by 
+* any person or entity by or on your behalf, exclusively with Microchip,s 
+* proprietary products.  Your acceptance and/or use of this code constitutes 
+* agreement to the terms and conditions of this notice.
+*
+* CODE ACCOMPANYING THIS MESSAGE IS SUPPLIED BY MICROCHIP "AS IS".  NO 
+* WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT NOT LIMITED 
+* TO, IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS FOR A 
+* PARTICULAR PURPOSE APPLY TO THIS CODE, ITS INTERACTION WITH MICROCHIP,S 
+* PRODUCTS, COMBINATION WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION. 
+*
+* YOU ACKNOWLEDGE AND AGREE THAT, IN NO EVENT, SHALL MICROCHIP BE LIABLE, WHETHER 
+* IN CONTRACT, WARRANTY, TORT (INCLUDING NEGLIGENCE OR BREACH OF STATUTORY DUTY), 
+* STRICT LIABILITY, INDEMNITY, CONTRIBUTION, OR OTHERWISE, FOR ANY INDIRECT, SPECIAL, 
+* PUNITIVE, EXEMPLARY, INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, FOR COST OR EXPENSE OF 
+* ANY KIND WHATSOEVER RELATED TO THE CODE, HOWSOEVER CAUSED, EVEN IF MICROCHIP HAS BEEN 
+* ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE.  TO THE FULLEST EXTENT 
+* ALLOWABLE BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN ANY WAY RELATED TO 
+* THIS CODE, SHALL NOT EXCEED THE PRICE YOU PAID DIRECTLY TO MICROCHIP SPECIFICALLY TO 
+* HAVE THIS CODE DEVELOPED.
+*
+* You agree that you are solely responsible for testing the code and 
+* determining its suitability.  Microchip has no obligation to modify, test, 
+* certify, or support the code.
+*
+* REVISION HISTORY:
+*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+* Author            Date      Comments on this revision
+*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+* HV                09/30/05  First release of source file
+*
+*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+*
+* ADDITIONAL NOTES:
+* The user should check to ensure that the PC value retrieved from
+* the stack is a valid program memory address. IF it isn't then
+* the user may conclude that the address error trap was generated
+* because an invalid value was pushed into the stack and followed by a
+* a "RETFIE" or "RETURN" instruction.
+**********************************************************************/
+
+.include "p30f6014a.inc"
+
+.global __AddressError
+.global _AddrErrInstructionAddrLow, _AddrErrInstructionAddrHigh
+
+.section .bss
+_AddrErrInstructionAddrLow: .space 2
+_AddrErrInstructionAddrHigh: .space 2
+
+.section .text
+
+__AddressError:
+        mov     #_AddrErrInstructionAddrHigh, w1
+        pop     [w1--]                  ;Pop the Program Counter (PC) from the stack
+        pop     [w1++]                  ;Remember that the PC(PCH and PCL) is stacked
+        bclr    [w1], #7                ;along with the SRL byte and IPL3 bit from
+        inc     w1, w1                  ;CORCON. So we need to extract just the 24-bit
+        clr.b   [w1]                    ;PCH:PCL information from the stack
+        mov     #_AddrErrInstructionAddrLow, w1  ;Decrement that value by 2
+        mov     #2, w2                  ;Store the 24-bit result into 2x16-bit words
+        subr    w2, [w1], [w1++]        ;in RAM at- AddrErrInstructionAddrHigh:Low
+        clr     w2
+        subbr   w2, [w1], [w1]
+        bclr    INTCON1, #ADDRERR       ;Clear the trap flag
+StayTrappedAddrErr:                     ;Stay in this routine
+        bra     StayTrappedAddrErr
+        ;Place a breakpoint above and halt the Debugging process and view
+        ;the variables named: AddrErrInstructionAddrHigh:Low
+        retfie
+
+/*
+EOF
+*/
+
+
